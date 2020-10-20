e920f5bc78f5e7267cef6a7c656d00894c0bcf20
ohohoi <ohohoi@sookmyung.ac.kr>
2020-10-20 23:19:41 +0900

    Initial commit

---
+++ src/main/java/com/ohohoi/github_api_ex/GithubApiExApplication.java
@@ -0,0 +1,13 @@
+package com.ohohoi.github_api_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubApiExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohohoi/github_api_ex/GithubApiExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohohoi.github_api_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
